fdaca4c67ddce4516e43b8b4f23e45e8390646e2
Ubuntu <ubuntu@ip-172-31-11-118.us-east-2.compute.internal>
2021-08-27 14:59:18 +0000

    Add Hello World

---
+++ bin/hello.go
@@ -0,0 +1,18 @@
+/*
+  Hello World
+  To run 'go run hello.go'
+
+  Go is made up of tokens
+  [keyword, identifier, constant, string literal or symbol]
+
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// If a name is Capitialized then it is exported.
+	fmt.Println("Hello, World!")
+}
